e1b1c8c5e674c39bd9876f80e96179457d10ecb7
Soy Frank <ernestinversion@gmail.com>
2024-03-09 20:19:15 -0300

    actualizando app

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
       moneda:"Euro",
       cambio: 1,
     },{
-      moneda: "Peso Argentino",
-      cambio:118.6,
+      moneda: "Real Brasileño",
+      cambio:5.40,
     },{
       moneda:"Peso Colombiano",
       cambio:4543.5,
@@ -37,24 +37,35 @@ function App() {
 
   return (
     <>
-      <div className='caja'>
-        <label htmlFor="u0">{cambios[0].moneda}</label>
-        <input id='u0' type='number' value={cantidad} onChange={modificar}></input>
+       {/*<div className='caja'>
+          {cambios.map((cambio, index) => (
+            <div key={index}>
+              <label htmlFor={`u${index}`}>{cambio.moneda}</label>
+              <input id={`u${index}`} type='number' value={index === 0 ? cantidad : resultados[index - 1]} readOnly />
+            </div>
+          ))}
+       </div>*/}
+      <div className="container">
+        <h1 className="title">Casa de Cambio de Frank</h1>
+          <div className='caja'>
+            <label htmlFor="u0">{cambios[0].moneda}</label>
+            <input id='u0' type='number' value={cantidad} onChange={modificar}></input>
 
-        <label htmlFor="u1">{cambios[1].moneda}</label>
-        <input id='u1' type='number' value={resultado1} readOnly></input>
+            <label htmlFor="u1">{cambios[1].moneda}</label>
+            <input id='u1' type='number' value={resultado1} readOnly></input>
 
-        <label htmlFor="u2">{cambios[2].moneda}</label>
-        <input id='u2' type='number' value={resultado2} readOnly></input>
+            <label htmlFor="u2">{cambios[2].moneda}</label>
+            <input id='u2' type='number' value={resultado2} readOnly></input>
 
-        <label htmlFor="u3">{cambios[3].moneda}</label>
-        <input id='u3' type='number' value={resultado3} readOnly></input>
+            <label htmlFor="u3">{cambios[3].moneda}</label>
+            <input id='u3' type='number' value={resultado3} readOnly></input>
 
-        <label htmlFor="u4">{cambios[4].moneda}</label>
-        <input id='u4' type='number' value={resultado4} readOnly></input>
+            <label htmlFor="u4">{cambios[4].moneda}</label>
+            <input id='u4' type='number' value={resultado4} readOnly></input>
 
-      </div>  
-    </>
+          </div>
+       </div> 
+      </>
   );
 }
 
